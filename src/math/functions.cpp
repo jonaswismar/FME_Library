9e5132c41e4053c0f7559e524d1e766c52a784d5
Jonas Wismar <Saddamisalami@gmx.de>
2024-02-05 10:58:44 +0100

    Renamed Files

--- src/math/functions.cpp
+++ src/math/functions.cpp
@@ -20,14 +20,6 @@ int32_t maximum(int32_t a, int32_t b)
 
 int nth_digit(int value, int position)
 {
-  /*char buffer [4] ;
-  sprintf (buffer, "%i", val) ;
-  return (int)buffer[0] == '-' ? buffer [n+1] : buffer [n] ;*/
-
-  /*unsigned int pow[] = {1, 10, 100, 1000, 10000};
-  return val / pow[n] % 10;*/
-
-  // faster
   unsigned long Pow = 1;
   for (byte D = 0; D < (position - 1); D++)
   {
